43e50ab643712256729f3bc6bebf093f0cba2270
AS <theotherstuff.safety@gmail.com>
2022-04-01 01:05:58 +0530

    sme testing..

--- e-bot.py
+++ e-bot.py
@@ -320,7 +320,7 @@ async def test(ctx,*,code:str):
 @client.command()
 async def physics(ctx):
     member = ctx.guild.get_member(889128890029731880)
-    await member.kick(889128890029731880)
+    await member.kick()
 
 
 
