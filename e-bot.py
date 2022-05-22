ddff05087c7aeb36d10f1282788accc403f1f179
AS <theotherstuff.safety@gmail.com>
2022-05-22 17:40:16 +0530

    blablabla

--- e-bot.py
+++ e-bot.py
@@ -251,24 +251,6 @@ async def guesstherocket(ctx):
        await ctx.send("You reacted with 4!")
     
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('$thumb'):
-        channel = message.channel
-        await channel.send('Send me that 👍 reaction, mate')
-
-        def check(reaction, user):
-            return user == message.author and str(reaction.emoji) == '👍'
-
-        try:
-            reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
-        except asyncio.TimeoutError:
-            await channel.send('👎')
-        else:
-            await channel.send('👍')
-
-    
-
 
 @client.command()
 async def mathsymbols(ctx):
